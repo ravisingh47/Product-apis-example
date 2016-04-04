86f63575dc56d8545216eecf614b4540c67f0b71
ravisingh47 <ravi.singh@bizongo.in>
2016-04-05 01:28:27 +0530

    Outputing rspec to documentation format

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,6 +40,8 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.default_formatter = 'doc'
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
@@ -68,9 +70,11 @@ RSpec.configure do |config|
   if config.files_to_run.one?
     # Use the documentation formatter for detailed output,
     # unless a formatter has already been configured
-    # (e.g. via a command-line flag).
+     (e.g. via a command-line flag).
     config.default_formatter = 'doc'
   end
+
+  config.default_formatter = 'doc'
 
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
