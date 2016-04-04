a80940c4ddcf5b1ff90e8beb7e32d1b560ea8f44
ravisingh47 <ravi.singh@bizongo.in>
2016-04-05 01:29:13 +0530

    Implementing BCrypt encrytion password for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  # storing encrypted password in self.encrypted_password
+  include BCrypt
+
+  def password
+    Password.new(encrypted_password)
+  end
+
+  def password=(new_password)
+    self.encrypted_password = Password.create(new_password)
+  end
 end
