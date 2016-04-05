fd7f7e340ef92c82554594276e41e5f33180fbd8
ravisingh47 <ravi.singh@bizongo.in>
2016-04-05 09:42:17 +0530

    Adding genrating auth token method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,27 @@ class User < ActiveRecord::Base
   # storing encrypted password in self.encrypted_password
   include BCrypt
 
+  before_create :generate_auth_token
+
   def password
     Password.new(encrypted_password)
   end
 
   def password=(new_password)
     self.encrypted_password = Password.create(new_password)
+  end
+
+  private
+
+  def generate_auth_token
+    loop do
+      token = SecureRandom.hex
+      user = User.find_by(auth_token: token)
+
+      if user.blank?
+        self.auth_token = token
+        break
+      end
+    end
   end
 end
