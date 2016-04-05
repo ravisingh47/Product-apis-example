4be59e6265bff295a86b263d33fe1dacea46c824
ravisingh47 <ravi.singh@bizongo.in>
2016-04-05 09:40:30 +0530

    Adding spec for generating auth token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,4 +34,33 @@ RSpec.describe User, :type => :model do
       end
     end
   end
+
+  describe "#generate_auth_token" do
+    let(:users) { create_list :user, 50 }
+
+    let(:sample_request) do
+      {
+        first_name: "Bruce",
+        last_name: "Wayne",
+        email: "bruce@gotham.com",
+        password: "iambatman",
+        mobile_number: "8603740623"
+      }
+    end
+
+    before :each do
+      @new_user = User.create(sample_request)
+    end
+
+    it "allways creates an auth_token" do
+      expect(@new_user.auth_token).not_to be_blank
+    end
+
+    it "creates an unique auth token for user" do
+      user_with_common_token = User
+                               .where(auth_token: @new_user.auth_token)
+    
+      expect(user_with_common_token.count).to eq(1)
+    end
+  end
 end
