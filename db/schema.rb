9e4f5f88e1b4ae5bc62a386b43722a61f18c3832
ravisingh47 <ravi.singh@bizongo.in>
2016-04-05 09:41:04 +0530

    Schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403172322) do
+ActiveRecord::Schema.define(version: 20160404202425) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160403172322) do
     t.string   "encrypted_password"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "auth_token"
   end
 
 end
